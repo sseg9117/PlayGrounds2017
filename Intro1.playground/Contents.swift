a4cf99179cce1d9c99444031cfd61f941c3cd11e
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-25 09:36:02 -0600

    added a strings thingy

---
+++ Intro1.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//Explictit Type declaration with no value
+var ramen :String
+//Implicit Type declaration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge =  myAge + 2135122345923
+var someValue = 3.145 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Sheradon"
+print(myName)
+//myName = soup
+//Can't assign into a let once assigned
+var bmw = "String"
+var someCar = "bmw"
+bmw = someCar
+//error ..print(luxury)
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import UIView
+import UIButton
+import UILabel
+import UIDatePicker
+
+var str = "Hello, playground"
