adc9928b1e24e7908d0baa7781f22b6a3ea145ec
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-27 08:09:17 -0600

    adding a metods playground to make sure i learn about swift.

---
+++ WorkingWithMethods.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public func simpleMethod() -> Void
+{
+    print("Sweet it is going to be 70+ this weekend")
+}
+simpleMethod()
+public func notSoSimpleMethod(name :String) -> Void
+{
+    print(name + " is the niftiest")
+}
+notSoSimpleMethod(name: "Sheradon Segota")
+public func evenLessSimpleMethod(count :Int,bandname :String, date :String) -> Void
+{
+    let info = "i have seen " + bandname + " \(count) times since \(date)"
+    print(info)
+}
+evenLessSimpleMethod(count: 4, bandname: "They might be Giants", date: "June, 1992")
+
+
+
+
+
+
+
+
+
