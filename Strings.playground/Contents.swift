fc768084ca692e9965b195b77141c57d54089f54
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-28 08:38:34 -0600

    added counting chracters.

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -1,8 +1,13 @@
 //: Playground - noun: a place where people can play
+
+
+
 //string interpolation
 let multiplier = 3
 let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
 //message is "3 times 2.5 is 7.5"
+
+
 
 //Counting Characters
 let unusualMenagerie = "Koala , Snail , Penguin , Dromedar "
@@ -12,5 +17,8 @@ print("the number of character in \(word) is \(word.characters.count)")
 //Prints "the number of cacters in cafe is 4"
 
 word += "\u{301}"  //COMBINING ACUTE ACCENT, U+0301
+
+print("the number of character in \(word) is \(word.characters.count)")
+//Prints "the number of character in cafe is 4"
 
 
