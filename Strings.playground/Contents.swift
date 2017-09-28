04385d9a2c2bc5d4924acf0467a13926a8f6ee00
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-28 08:35:58 -0600

    doing strings assignment no done

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -1,9 +1,16 @@
 //: Playground - noun: a place where people can play
+//string interpolation
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+//message is "3 times 2.5 is 7.5"
 
-import UIKit
-import UIView
-import UIButton
-import UILabel
-import UIDatePicker
+//Counting Characters
+let unusualMenagerie = "Koala , Snail , Penguin , Dromedar "
+print ("unusualMenagerie has \(unusualMenagerie.characters.count) character")
+var word = "cafe"
+print("the number of character in \(word) is \(word.characters.count)")
+//Prints "the number of cacters in cafe is 4"
 
-var str = "Hello, playground"
+word += "\u{301}"  //COMBINING ACUTE ACCENT, U+0301
+
+
